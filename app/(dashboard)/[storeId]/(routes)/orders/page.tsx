1f786efdca77074400dac96ed53d9980f5315e1e
manyuvraj-sandhu <manyuvraj.sandhu@gmail.com>
2024-08-30 21:44:00 -0400

    orders entity progress today

--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -1,9 +1,10 @@
 import { format } from "date-fns";
 
 import prismadb from "@/lib/prismadb";
+import { formatter } from "@/lib/utils";
 
-import { BillboardClient } from "./components/client";
-import { BillboardColumn } from "./components/columns";
+import { OrderClient } from "./components/client";
+import { OrderColumn } from "./components/columns";
 
 const OrdersPage = async ({
     params
@@ -26,16 +27,22 @@ const OrdersPage = async ({
         }
     });
 
-    const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    const formattedOrders: OrderColumn[] = orders.map((item) => ({
         id: item.id,
-        label: item.label,
+        phone: item.phone,
+        address: item.address,
+        products: item.orderItems.map((orderItem) => orderItem.product.name).join(', '),
+        totalPrice: formatter.format(item.orderItems.reduce((total, item) => {
+            return total + Number(item.product.price)
+        }, 0)),
+        isPaid: item.isPaid,
         createdAt: format(item.createdAt, "MMMM do, yyyy")
     }));
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardClient data={formattedBillboards} />
+                <OrderClient data={formattedOrders} />
             </div>
         </div>
     );
